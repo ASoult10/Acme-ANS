6e4995d81a26738e7559330a357ea4021986af4a
Alejandro Soult <asoulttoscano@gmail.com>
2025-02-28 15:19:36 +0100

    Creación boceto primero de Servicio

---
+++ src/main/java/acme/entities/service/Service.java
@@ -0,0 +1,52 @@
+
+package acme.entities.service;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoney;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Service extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(min = 1, max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidUrl
+	// TODO: Store where?
+	private String				link;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	private Integer				dWellTime;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z]{4}-[0-9]{2}$")
+	@Column(unique = true)
+	// TODO: Two lasts digits -> Current year?
+	private String				promoteCode;
+
+	@Mandatory
+	@ValidMoney
+	private Money				moneyDiscounted;
+}
