3f08d9b28063e674465ad16cb491e9d4f1c18695
Alejandro Soult <asoulttoscano@gmail.com>
2025-02-28 18:25:25 +0100

    Creación paquetes y clases para Customer, Booking, Passenger

---
+++ src/main/java/acme/entities/booking/Booking.java
@@ -0,0 +1,14 @@
+
+package acme.entities.booking;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Booking {
+
+}
---
+++ src/main/java/acme/entities/passenger/Passenger.java
@@ -0,0 +1,14 @@
+
+package acme.entities.passenger;
+
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Passenger {
+
+}
