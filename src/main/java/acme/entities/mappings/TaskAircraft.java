88ac4a855a37dba5f85eb676ff596dd49c733702
Enrique Nicolae Barac Ploae <enriquenicolaebarac55@gmail.com>
2025-03-11 09:46:42 +0100

    Comentadas clases intermedias por problemas y subido aircraft a main

--- src/main/java/acme/entities/mappings/TaskAircraft.java
+++ src/main/java/acme/entities/mappings/TaskAircraft.java
@@ -1,26 +1,31 @@
-
-package acme.entities.mappings;
-
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-
-import acme.client.components.basis.AbstractEntity;
-import acme.client.components.validation.Mandatory;
-import acme.entities.aircrafts.Aircraft;
-import acme.entities.tasks.Task;
-
-public class TaskAircraft extends AbstractEntity {
-
-	private static final long	serialVersionUID	= 1L;
-
-	@Mandatory
-	@Valid
-	@ManyToOne(optional = false)
-	private Aircraft			aircraft;
-
-	@Mandatory
-	@Valid
-	@ManyToOne(optional = false)
-	private Task				task;
-
-}
+/*
+ * package acme.entities.mappings;
+ * 
+ * import javax.persistence.ManyToOne;
+ * import javax.validation.Valid;
+ * 
+ * import acme.client.components.basis.AbstractEntity;
+ * import acme.client.components.validation.Mandatory;
+ * import acme.entities.aircrafts.Aircraft;
+ * import acme.entities.tasks.Task;
+ * 
+ * public class TaskAircraft extends AbstractEntity {
+ * 
+ * private static final long serialVersionUID = 1L;
+ * 
+ * @Mandatory
+ * 
+ * @Valid
+ * 
+ * @ManyToOne(optional = false)
+ * private Aircraft aircraft;
+ * 
+ * @Mandatory
+ * 
+ * @Valid
+ * 
+ * @ManyToOne(optional = false)
+ * private Task task;
+ * 
+ * }
+ */
