e45ae61e36ee1e7f0fe2bf69952b81843b39807e
Enrique Nicolae Barac Ploae <enriquenicolaebarac55@gmail.com>
2025-03-26 13:16:07 +0100

    repositorios actualizados

--- src/main/java/acme/features/administrator/aircraft/AircraftRepository.java
+++ src/main/java/acme/features/administrator/aircraft/AircraftRepository.java
@@ -1,7 +1,6 @@
 
 package acme.features.administrator.aircraft;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
@@ -13,9 +12,7 @@ import acme.entities.aircrafts.Aircraft;
 @Repository
 public interface AircraftRepository extends AbstractRepository {
 
-	Optional<Aircraft> findOneAircraftByRegistrationNumber(String registrationNumber);
-
 	@Query("select a from Aircraft a where a.registrationNumber = :registrationNumber")
-	List<Aircraft> findAircraftsByRegistrationNumber(String registrationNumber);
+	Optional<Aircraft> findOneAircraftByRegistrationNumber(String registrationNumber);
 
 }
--- src/main/java/acme/features/authenticated/technician/TechnicianRepository.java
+++ src/main/java/acme/features/authenticated/technician/TechnicianRepository.java
@@ -1,7 +1,6 @@
 
 package acme.features.authenticated.technician;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +12,7 @@ import acme.realms.Technician;
 @Repository
 public interface TechnicianRepository extends AbstractRepository {
 
+	@Query("select t from Technician t where t.licenseNumber = :licenseNumber")
 	Optional<Technician> findOneTechnicianByLicenseNumber(String licenseNumber);
 
-	@Query("select t from Technician t where t.licenseNumber = :licenseNumber")
-	List<Technician> findTechniciansByLicenseNumber(String licenseNumber);
 }
