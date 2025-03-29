3d9b33262bd19078c73c09eed6b620a2005027e0
Alejandro Soult <asoulttoscano@gmail.com>
2025-03-29 19:10:05 +0100

    Cambios menores

--- src/main/java/acme/features/customer/booking/CustomerPassengerPublishService.java
+++
@@ -1,6 +0,0 @@
-package acme.features.customer.booking;
-
-
-public class CustomerPassengerPublishService {
-
-}
---
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerPublishService.java
@@ -0,0 +1,10 @@
+
+package acme.features.customer.passenger;
+
+import acme.client.services.AbstractGuiService;
+import acme.entities.passenger.Passenger;
+import acme.realms.Customer;
+
+public class CustomerPassengerPublishService extends AbstractGuiService<Customer, Passenger> {
+
+}
