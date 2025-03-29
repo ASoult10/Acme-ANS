46478ada83ad3ea3b79afe1d03fe698a989e1ae7
Enrique Nicolae Barac Ploae <enriquenicolaebarac55@gmail.com>
2025-03-29 20:51:23 +0100

    falta publish de task, y arreglar MR create, delete de involvedIn

---
+++ src/main/java/acme/features/technician/task/TechnicianTaskUpdateService.java
@@ -0,0 +1,83 @@
+
+package acme.features.technician.task;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.tasks.Task;
+import acme.entities.tasks.TaskStatus;
+import acme.realms.Technician;
+
+@GuiService
+public class TechnicianTaskUpdateService extends AbstractGuiService<Technician, Task> {
+
+	@Autowired
+	private TechnicianTaskRepository repository;
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Task task;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		task = this.repository.findTaskById(id);
+
+		super.getBuffer().addData(task);
+	}
+
+	@Override
+	public void bind(final Task task) {
+		int technicianId;
+		Technician technician;
+
+		technicianId = super.getRequest().getData("technician", int.class);
+		technician = this.repository.findTechnicianById(technicianId);
+
+		super.bindObject(task, "type", "description", "priority", "estimatedDuration");
+		task.setTechnician(technician);
+	}
+
+	@Override
+	public void validate(final Task task) {
+		;
+	}
+
+	@Override
+	public void perform(final Task task) {
+		this.repository.save(task);
+	}
+
+	@Override
+	public void unbind(final Task task) {
+		SelectChoices taskStatus;
+		SelectChoices technicianChoices;
+
+		Collection<Technician> technicians;
+		Dataset dataset;
+
+		technicians = this.repository.findAllTechnicians();
+		technicianChoices = SelectChoices.from(technicians, "licenseNumber", task.getTechnician());
+
+		taskStatus = SelectChoices.from(TaskStatus.class, task.getType());
+
+		dataset = super.unbindObject(task, "type", "description", "priority", "estimatedDuration");
+		dataset.put("confirmation", false);
+		dataset.put("readonly", false);
+		dataset.put("taskStatus", taskStatus);
+		dataset.put("technicians", technicianChoices);
+		dataset.put("technician", technicianChoices.getSelected().getKey());
+
+		super.getResponse().addData(dataset);
+	}
+}
--- src/main/resources/WEB-INF/views/fragments/menu.jsp
+++ src/main/resources/WEB-INF/views/fragments/menu.jsp
@@ -58,6 +58,7 @@
 		
 		<acme:menu-option code="master.menu.technician" access="hasRealm('Technician')">
 			<acme:menu-suboption code="master.menu.technician.list-maintenanceRecord" action="/technician/maintenance-record/list"/>
+			<acme:menu-suboption code="master.menu.technician.list-task" action="/technician/task/list"/>
 		</acme:menu-option>
 		
 	</acme:menu-left>
