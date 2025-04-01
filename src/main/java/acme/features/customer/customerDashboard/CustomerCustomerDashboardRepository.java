6d3d5b6eb00aec90e23fda7d9b080a0e87505f77
Alejandro Soult <asoulttoscano@gmail.com>
2025-04-01 16:10:42 +0200

    Cambios sutiles

--- src/main/java/acme/features/customer/customerDashboard/CustomerCustomerDashboardRepository.java
+++ src/main/java/acme/features/customer/customerDashboard/CustomerCustomerDashboardRepository.java
@@ -1,53 +1,57 @@
 
 package acme.features.customer.customerDashboard;
 
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.client.components.datatypes.Money;
 import acme.client.repositories.AbstractRepository;
 
 @Repository
 public interface CustomerCustomerDashboardRepository extends AbstractRepository {
 
-	//	@Query("")
-	//	List<String> lastFiveDestinations(Integer customerId);
-	//
-	//	@Query("")
-	//	Money spentMoney(Integer customerId);
-	//
-	//	@Query("")
-	//	Integereger economyBookings(Integer customerId);
-	//
-	//	@Query("")
-	//	Integereger businessBookings(Integer customerId);
-	//
-	//	@Query("")
-	//	Money bookingTotalCost(Integer customerId);
-	//
-	//	@Query("")
-	//	Money bookingAverageCost(Integer customerId);
-	//
-	//	@Query("")
-	//	Money bookingMinimumCost(Integer customerId);
-	//
-	//	@Query("")
-	//	Money bookingMaximumCost(Integer customerId);
-	//
-	//	@Query("")
-	//	Money bookingDeviationCost(Integer customerId);
-	//
-	//	@Query("")
-	//	Integereger bookingTotalPassengers(Integer customerId);
-	//
-	//	@Query("")
-	//	Double bookingAveragePassengers(Integer customerId);
-	//
-	//	@Query("")
-	//	Integereger bookingMinimumPassengers(Integer customerId);
-	//
-	//	@Query("")
-	//	Integereger bookingMaximumPassengers(Integer customerId);
-	//
-	//	@Query("")
-	//	Double bookingDeviationPassengers(Integer customerId);
+	@Query("")
+	Collection<String> lastFiveDestinations(Integer customerId);
+
+	@Query("")
+	Money spentMoney(Integer customerId);
+
+	@Query("")
+	Integer economyBookings(Integer customerId);
+
+	@Query("")
+	Integer businessBookings(Integer customerId);
+
+	@Query("")
+	Money bookingTotalCost(Integer customerId);
+
+	@Query("")
+	Money bookingAverageCost(Integer customerId);
+
+	@Query("")
+	Money bookingMinimumCost(Integer customerId);
+
+	@Query("")
+	Money bookingMaximumCost(Integer customerId);
+
+	@Query("")
+	Money bookingDeviationCost(Integer customerId);
+
+	@Query("")
+	Integer bookingTotalPassengers(Integer customerId);
+
+	@Query("")
+	Double bookingAveragePassengers(Integer customerId);
+
+	@Query("")
+	Integer bookingMinimumPassengers(Integer customerId);
+
+	@Query("")
+	Integer bookingMaximumPassengers(Integer customerId);
+
+	@Query("")
+	Double bookingDeviationPassengers(Integer customerId);
 
 }
