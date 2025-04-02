904beb8073309d1492c629eb06be800446380e75
Alejandro Soult <asoulttoscano@gmail.com>
2025-04-02 12:09:01 +0200

    Primer boceto de querys para customer dashboard

--- src/main/java/acme/features/customer/customerDashboard/CustomerCustomerDashboardRepository.java
+++ src/main/java/acme/features/customer/customerDashboard/CustomerCustomerDashboardRepository.java
@@ -8,46 +8,46 @@ import acme.client.repositories.AbstractRepository;
 @Repository
 public interface CustomerCustomerDashboardRepository extends AbstractRepository {
 
-	//	@Query("")
+	//	@Query("SELECT DISTINCT f.destinationCity FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId ORDER BY b.purchaseMoment DESC")
 	//	Collection<String> lastFiveDestinations(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(SUM(f.cost.amount * (SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId AND FUNCTION('YEAR', b.purchaseMoment) = FUNCTION('YEAR', CURRENT_DATE)")
 	//	Money spentMoney(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COUNT(b) FROM Booking b WHERE b.customer.id = :customerId AND b.travelClass = ECONOMY")
 	//	Integer economyBookings(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COUNT(b) FROM Booking b WHERE b.customer.id = :customerId AND b.travelClass = BUSINESS")
 	//	Integer businessBookings(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(SUM(f.cost.amount * (SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId AND b.purchaseMoment >= CURRENT_DATE - 5 * 365")
 	//	Money bookingTotalCost(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(AVG(f.cost.amount * (SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId AND b.purchaseMoment >= CURRENT_DATE - 5 * 365")
 	//	Money bookingAverageCost(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(MIN(f.cost.amount * (SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId AND b.purchaseMoment >= CURRENT_DATE - 5 * 365")
 	//	Money bookingMinimumCost(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(MAX(f.cost.amount * (SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId AND b.purchaseMoment >= CURRENT_DATE - 5 * 365")
 	//	Money bookingMaximumCost(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(STDDEV(f.cost.amount * (SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b JOIN b.flight f WHERE b.customer.id = :customerId AND b.purchaseMoment >= CURRENT_DATE - 5 * 365")
 	//	Money bookingDeviationCost(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(SUM((SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b WHERE b.customer.id = :customerId")
 	//	Integer bookingTotalPassengers(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(AVG((SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b WHERE b.customer.id = :customerId")
 	//	Double bookingAveragePassengers(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(MIN((SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b WHERE b.customer.id = :customerId")
 	//	Integer bookingMinimumPassengers(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(MAX((SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b WHERE b.customer.id = :customerId")
 	//	Integer bookingMaximumPassengers(Integer customerId);
 	//
-	//	@Query("")
+	//	@Query("SELECT COALESCE(STDDEV((SELECT COUNT(bp) FROM BookingPassenger bp WHERE bp.booking.id = b.id)), 0) FROM Booking b WHERE b.customer.id = :customerId")
 	//	Double bookingDeviationPassengers(Integer customerId);
 
 }
