9c5f6a05cf0e79b79536eeea24ba9914d7ac51d7
Alejandro Soult <asoulttoscano@gmail.com>
2025-04-25 11:42:49 +0200

    Cambio de manera de obtener API con métodos de Spring

--- src/main/java/acme/features/customer/recommendationDashboard/CustomerRecommendationDashboardListService.java
+++ src/main/java/acme/features/customer/recommendationDashboard/CustomerRecommendationDashboardListService.java
@@ -1,15 +1,17 @@
 
 package acme.features.customer.recommendationDashboard;
 
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
@@ -34,7 +36,6 @@ public class CustomerRecommendationDashboardListService extends AbstractGuiServi
 		super.getResponse().setAuthorised(status);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public void load() {
 
@@ -42,44 +43,37 @@ public class CustomerRecommendationDashboardListService extends AbstractGuiServi
 
 		String city = super.getRequest().getData("city", String.class);
 		String country = super.getRequest().getData("country", String.class);
-		String apiKey = "";
+		String apiKey = "kv47Ard-jTcCTDgXI6mCtoZUIR3X_TTg2-DtAv5paENfz_WZvFiwn8zH2LNZZ-lTcZJtVyGAU5btj4KA5oWEbcAplnoeQ1qzRQtzMbm_eSaJlR6mThHF9x9HbCD5Z3Yx";
 		String term = "tourist";
 		Integer count = 5;
 
 		try {
-			String urlString = String.format("https://api.yelp.com/v3/businesses/search?location=%s&term=%s&limit=%d", city, term, count);
-			URL url = new URL(urlString);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Authorization", "Bearer " + apiKey);
+			String url = "https://api.yelp.com/v3/businesses/search?location={0}&term={1}&limit={2}";
 
-			int responseCode = conn.getResponseCode();
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("Authorization", "Bearer " + apiKey);
+			HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
 
-			if (responseCode == 200) {
-				InputStream contStream = conn.getInputStream();
-				byte[] contBytes = contStream.readAllBytes();
-				String contString = "";
+			RestTemplate restTemplate = new RestTemplate();
+			ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class, city, term, count);
 
-				for (byte bytte : contBytes)
-					contString += (char) bytte;
+			assert response != null && response.getBody() != null;
 
-				JSONObject contJSON = new JSONObject(contString);
-				JSONArray businesses = (JSONArray) contJSON.get("businesses");
+			JSONObject contJSON = new JSONObject(response.getBody());
+			JSONArray businesses = contJSON.getJSONArray("businesses");
 
-				for (int i = 0; i < count; i++) {
-					JSONObject iObj = (JSONObject) businesses.get(i);
-					RecommendationDashboard recommendationDashboard = new RecommendationDashboard();
-					recommendationDashboard.setCity(city);
-					recommendationDashboard.setCountry(country);
-					recommendationDashboard.setName(iObj.get("name").toString());
-					JSONArray categoryArray = (JSONArray) iObj.get("categories");
-					recommendationDashboard.setRecommendationType(((JSONObject) categoryArray.get(0)).get("title").toString());
-					recommendationDashboards.add(recommendationDashboard);
-				}
+			for (int i = 0; i < count && i < businesses.length(); i++) {
+				JSONObject iObj = businesses.getJSONObject(i);
+				RecommendationDashboard recommendationDashboard = new RecommendationDashboard();
+				recommendationDashboard.setCity(city);
+				recommendationDashboard.setCountry(country);
+				recommendationDashboard.setName(iObj.getString("name"));
+				JSONArray categoryArray = iObj.getJSONArray("categories");
+				recommendationDashboard.setRecommendationType(categoryArray.getJSONObject(0).getString("title"));
+				recommendationDashboards.add(recommendationDashboard);
+			}
 
-			} else
-				System.out.println("Request Error: Code " + responseCode);
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			e.printStackTrace();
 			return;
 		}
@@ -90,6 +84,8 @@ public class CustomerRecommendationDashboardListService extends AbstractGuiServi
 	@Override
 	public void unbind(final RecommendationDashboard recommendationsDashboard) {
 		Dataset dataset = super.unbindObject(recommendationsDashboard, "city", "country", "name", "description", "recommendationType");
+		dataset.put("cityName", recommendationsDashboard.getCity());
+		dataset.put("countryName", recommendationsDashboard.getCountry());
 		super.getResponse().addData(dataset);
 	}
 
