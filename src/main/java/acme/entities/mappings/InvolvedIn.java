c380cc670659da576a221f6995d36408a8ed0ecc
Enrique Nicolae Barac Ploae <enriquenicolaebarac55@gmail.com>
2025-06-21 17:32:40 +0200

    indices borrado y añadidos

--- src/main/java/acme/entities/mappings/InvolvedIn.java
+++ src/main/java/acme/entities/mappings/InvolvedIn.java
@@ -2,7 +2,9 @@
 package acme.entities.mappings;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -15,6 +17,10 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "maintenanceRecord_id"), @Index(columnList = "task_id")
+})
+
 public class InvolvedIn extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
